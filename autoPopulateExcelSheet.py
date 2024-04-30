b0a01f5864d16068808c474ed1134af4ac89924c
joeythehaxer <112483823+joeythehaxer@users.noreply.github.com>
2024-04-30 15:36:37 +0100

    proper indexing and positioning added

--- autoPopulateExcelSheet.py
+++ autoPopulateExcelSheet.py
@@ -1,17 +1,25 @@
 import os
-import openpyxl
-from striprtf.striprtf import rtf_to_text
 import re
+from openpyxl import load_workbook
+from striprtf import striprtf
 from langdetect import detect
 
 # Constants
-FOLDER_PATH = r"C ACTIONS TO ADD"
-EXCEL_FILE_PATH = r"New Microsoft Excel Worksheet.xlsx"
+FOLDER_PATH = "C ACTIONS TO ADD"
+EXCEL_FILE_PATH = "New Microsoft Excel Worksheet.xlsx"
 SHEET_NAME = "Sheet1"
 
 
 def remove_non_english(text):
+    """
+    Remove non-English words from the given text.
 
+    Args:
+    text (str): Input text containing words.
+
+    Returns:
+    str: Text with only English words.
+    """
     # Tokenize the text into words
     words = re.findall(r'\b\w+\b', text)
 
@@ -25,6 +33,15 @@ def remove_non_english(text):
 
 
 def summarize(text):
+    """
+    Summarize the text by removing non-English words.
+
+    Args:
+    text (str): Input text.
+
+    Returns:
+    str: Summarized text with only English words.
+    """
     # Remove non-English text
     english_text = remove_non_english(text)
 
@@ -32,153 +49,139 @@ def summarize(text):
     return english_text
 
 
-def rephrase(text):
-    # Split the text into sentences
-    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+def load_excel_workbook():
+    """
+    Load the Excel workbook from the specified path.
 
-    # Rephrase each sentence
-    rephrased_sentences = []
-    for sentence in sentences:
-        # Split the sentence into words
-        words = sentence.split()
-
-        # Remove non-English words
-        english_words = [word for word in words]
-
-        # Join the English words into a sentence
-        rephrased_sentence = ' '.join(english_words)
-
-        rephrased_sentences.append(rephrased_sentence)
-
-    # Join the rephrased sentences into a single string
-    rephrased_text = ' '.join(rephrased_sentences)
-
-    return rephrased_text
-
-
-def load_excel_workbook(excel_file_path):
-    return openpyxl.load_workbook(excel_file_path)
+    Returns:
+    openpyxl.workbook.Workbook: Loaded Excel workbook object.
+    """
+    return load_workbook(EXCEL_FILE_PATH)
 
 
 def find_last_empty_row(ws):
+    """
+    Find the last empty row in the specified worksheet.
+
+    Args:
+    ws (openpyxl.worksheet.Worksheet): Worksheet object.
+
+    Returns:
+    int: Index of the last empty row.
+    """
     return ws.max_row + 1
 
 
 def extract_info_from_rtf(file_path):
-    extracted_info = []
+    """
+    Extract information from the RTF file.
+
+    Args:
+    file_path (str): Path to the RTF file.
+
+    Returns:
+    list: Extracted information.
+    """
     with open(file_path, 'r') as f:
         rtf_content = f.read()
-        plain_text = rtf_to_text(rtf_content)
-        lines = plain_text.split("|")
-        clientraw = lines[0].split(">")[1]
-        client = ""
-        for char in clientraw:
-            if char.isalnum() or char == " ":
-                client += char
-        clientDerivative = lines[50]
-        jobString = "3197"
-        poNumberString = lines[2]
-        addressString = lines[23]
-        postcodePattern = re.compile(r'\b[A-Z]{1,2}\d{1,2}\s\d[A-Z]{2}\b')
-        riskString = ""
-        try:
-            postcodeString = postcodePattern.findall(addressString)[0]
-        except IndexError or UnboundLocalError:
-            postcodeString = "None"
-        telString = "NOT SUPPLIED"
-        if lines[16] != "":
-            telString = lines[16]
-        dateRaisedString = lines[38]
-        targetDateString = lines[46]
-        tenderNumberPattern = re.compile(r'\b\d{5,7}\b')
-        tenderNumberList = tenderNumberPattern.findall(lines[61])
+        plain_text = striprtf.rtf_to_text(rtf_content)
 
-        # print(tenderNumberList)
-        try:
-            tenderNumberString = tenderNumberList[0]
-        except IndexError:
-            tenderNumberString = "None"
-        rawActionDescriptionString = lines[61]
-        englishActionString = summarize(rawActionDescriptionString)
+    lines = plain_text.split("|")
+    client_raw = lines[0].split(">")[1]
+    client = "".join(char for char in client_raw if char.isalnum() or char == " ")
+    client_derivative = lines[50]
+    job_string = "3197"
+    po_number_string = lines[2]
+    address_string = lines[23]
+    postcode_pattern = re.compile(r'\b[A-Z]{1,2}\d{1,2}\s\d[A-Z]{2}\b')
+    risk_string = ""
 
+    try:
+        postcode_string = postcode_pattern.findall(address_string)[0]
+    except IndexError or UnboundLocalError:
+        postcode_string = "None"
 
-        if len(tenderNumberList) <= 1:
-            extracted_info.append(clientDerivative)
-            extracted_info.append(jobString)
-            extracted_info.append(poNumberString)
-            extracted_info.append(tenderNumberString)
-            extracted_info.append(dateRaisedString)
-            extracted_info.append(targetDateString)
-            extracted_info.append(telString)
-            extracted_info.append(addressString)
-            extracted_info.append(postcodeString)
-            extracted_info.append(riskString)
-            extracted_info.append(englishActionString)
-            return extracted_info
-        else:
-            extracted_info.append(clientDerivative)
-            extracted_info.append(jobString)
-            extracted_info.append(poNumberString)
-            extracted_info.append(tenderNumberList)
-            extracted_info.append(dateRaisedString)
-            extracted_info.append(targetDateString)
-            extracted_info.append(telString)
-            extracted_info.append(addressString)
-            extracted_info.append(postcodeString)
-            extracted_info.append(riskString)
-            extracted_info.append(englishActionString)
-            return extracted_info
+    tel_string = "NOT SUPPLIED"
+    if lines[16]:
+        tel_string = lines[16]
+
+    date_raised_string = lines[38]
+    target_date_string = lines[46]
+    tender_number_pattern = re.compile(r'\b\d{5,7}\b')
+    tender_number_list = tender_number_pattern.findall(lines[61])
+
+    try:
+        tender_number_string = tender_number_list[0]
+    except IndexError:
+        tender_number_string = "None"
+
+    raw_action_description_string = lines[61]
+    english_action_string = summarize(raw_action_description_string)
+
+    if len(tender_number_list) <= 1:
+        extracted_info = [client_derivative, job_string, po_number_string, tender_number_string,
+                          date_raised_string, target_date_string, tel_string, address_string,
+                          postcode_string, risk_string, english_action_string]
+    else:
+        extracted_info = [client_derivative, job_string, po_number_string, tender_number_list,
+                          date_raised_string, target_date_string, tel_string, address_string,
+                          postcode_string, risk_string, english_action_string]
+
+    return extracted_info
 
 
 def paste_info_to_excel(ws, extracted_info, last_row):
-    reProcessedExtractedInfo = []
-    for i in extracted_info:
-        reProcessedExtractedInfo.append(i)
+    """
+    Paste the extracted information into the Excel worksheet.
 
-    if str(type(extracted_info[3])) != "<class \'list\'>":
-        for i, cell_text in enumerate(extracted_info):
-            ws.cell(row=last_row, column=i + 2).value = cell_text
-        return last_row + 1
-    else:
-        v = 0
-        for i in extracted_info[3]:
-            reProcessedExtractedInfo[3] = extracted_info[3][v]
-            # print(i)
-            for x, cell_text in enumerate(reProcessedExtractedInfo):
-                ws.cell(row=last_row, column=x + 2).value = cell_text
-            v += 1
+    Args:
+    ws (openpyxl.worksheet.Worksheet): Worksheet object.
+    extracted_info (list): Extracted information to paste.
+    last_row (int): Index of the last row in the worksheet.
+
+    Returns:
+    int: Updated index of the last row.
+    """
+    if isinstance(extracted_info[3], list):
+        for tender_number in extracted_info[3]:
+            extracted_info[3] = tender_number
+            for column, cell_text in enumerate(extracted_info, start=2):
+                ws.cell(row=last_row, column=column).value = cell_text
             last_row += 1
-        return last_row
+    else:
+        for column, cell_text in enumerate(extracted_info, start=2):
+            ws.cell(row=last_row, column=column).value = cell_text
+        last_row += 1
+
+    return last_row
 
 
-def save_excel_workbook(wb, excel_file_path):
-    wb.save(excel_file_path)
+def save_excel_workbook(wb):
+    """
+    Save the Excel workbook to the specified path.
+
+    Args:
+    wb (openpyxl.workbook.Workbook): Workbook object to save.
+    """
+    wb.save(EXCEL_FILE_PATH)
 
 
-def import_info_from_rtf_folder(folder_path, excel_file_path, sheet_name):
-    # Load Excel workbook
-    wb = load_excel_workbook(excel_file_path)
-    ws = wb[sheet_name]
+def import_info_from_rtf_folder():
+    """
+    Import information from RTF files in the specified folder and paste it into an Excel worksheet.
+    """
+    wb = load_excel_workbook()
+    ws = wb[SHEET_NAME]
 
-    # Loop through each RTF file in the folder
-    for filename in os.listdir(folder_path):
+    for filename in os.listdir(FOLDER_PATH):
         if filename.endswith(".rtf"):
-            # Construct the full file path
-            # print(filename)
-            file_path = os.path.join(folder_path, filename)
-
-            # Extract information from RTF file
+            file_path = os.path.join(FOLDER_PATH, filename)
             extracted_info = extract_info_from_rtf(file_path)
-
-            # Find the last empty row in Excel sheet
             last_row = find_last_empty_row(ws)
+            last_row = paste_info_to_excel(ws, extracted_info, last_row)
 
-            # Paste the extracted information into a new row in Excel
-            paste_info_to_excel(ws, extracted_info, last_row)
-
-    # Save changes to Excel workbook
-    save_excel_workbook(wb, excel_file_path)
+    save_excel_workbook(wb)
 
 
 # Usage
-import_info_from_rtf_folder(FOLDER_PATH, EXCEL_FILE_PATH, SHEET_NAME)
+import_info_from_rtf_folder()
